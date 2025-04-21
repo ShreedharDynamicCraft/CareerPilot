e17fb7f43a3676be5e58fbbc6ee57f398a405bd2
Shreedhar <shreedharanandji@gmail.com>
2025-04-22 00:07:36 +0530

    env file setup for hosting

--- backend/app.py
+++ backend/app.py
@@ -316,6 +316,4 @@ async def websocket_chat(websocket: WebSocket):
 
 
 if __name__ == "__main__":
-
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0")  # No need to specify port
